b328f2145919b071a57a68fe238f4806b1bd94f3
Simon Jesenko <simon.jesenko@gmail.com>
2015-03-17 22:59:10 +0100

    installation of spacemacs config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ ln -fs ~/dotfiles/.vimrc ~/.vimrc
 ln -fs ~/dotfiles/.vimrc.bundles ~/.vimrc.bundles
 ln -fs ~/dotfiles/.zshrc ~/.zshrc
 ln -fs ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -fs ~/dotfiles/.spacemacs ~/.spacemacs
 # needed for tmux
 brew install reattach-to-user-namespace
 # add ag global ignore pattern - needed for now as ag does not respect git ignore properly yet
